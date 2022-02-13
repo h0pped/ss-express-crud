601f74953edcd9b857eb944a4a80c1cdc9957346
h0pped <notawril@gmail.com>
2022-02-13 10:33:16 +0100

    init

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
+
+console.log(process.env.PORT);
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express Library API",
+    },
+    servers: [
+      {
+        url: "http://localhost:8000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+const swaggerDocs = swaggerJsDoc(options);
 const app = express();
-require("dotenv").config({ path: "./env/dev.env" });
+
+require("dotenv").config({ path: "./env/.env" });
+
+const PORT = process.env.port || 8000;
+const DB_URL = process.env.DB_URL;
 
 const userRouter = require("./routes/user.js");
 
 app.use(express.json());
 app.use("/user", userRouter);
 
-const PORT = process.env.port || 80;
-const DB_URL = process.env.DB_URL;
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * /:
+ *  get:
+ *    description: Use to check if server responses
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
+app.get("/", (req, res) => {
+  res.send({ msg: "Hello there!" });
+});
 app.listen(PORT, async () => {
   mongoose
     .connect(DB_URL, {
--- routes/user.js
+++ routes/user.js
@@ -5,14 +5,68 @@ const router = express.Router();
 
 // Middlewares
 const checkEmail = require("../middlewares/checkEmail.js");
-// const checkID = require("../middlewares/checkID.js");
-
 // Util
 // const findIndexByID = require("../util/findIndexByID.js");
 
 // Models
 const User = require("../models/user");
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - name
+ *         - surname
+ *         - email
+ *       properties:
+ *         _id:
+ *           type: string
+ *           description: The auto-generated id of the user
+ *         name:
+ *           type: string
+ *           description: First name of the user
+ *         surname:
+ *           type: string
+ *           description: Last name of the user
+ *         email:
+ *           type: string
+ *           description: email of the user
+ *       example:
+ *         _id: "112312sdfksdf"
+ *         name: John
+ *         surname: Doe
+ *         email: notawril@gmail.com
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Users
+ *  description: Users managing API
+ *
+ */
+
+/**
+ * @swagger
+ * /user:
+ *  get:
+ *    summary: Returns a list of users
+ *    tags: [Users]
+ *    description: Get list of users
+ *    responses:
+ *      '200':
+ *        description: List of users
+ *        schema:
+ *          type: array
+ *          items:
+ *            $ref: '#components/schemas/User'
+ *      '500':
+ *        description: Internal server error
+ *
+ */
 router.get("/", async (req, res) => {
   try {
     const users = await User.find();
@@ -22,6 +76,34 @@ router.get("/", async (req, res) => {
     return res.status(500).send({ err });
   }
 });
+
+/**
+ * @swagger
+ * /user/{id}:
+ *  get:
+ *    summary: Returns a User
+ *    tags: [Users]
+ *    description: Get User by ID
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *    responses:
+ *      '200':
+ *        description: User object
+ *        contens:
+ *          application/json:
+ *           schema:
+ *              items:
+ *                $ref: '#components/schemas/User'
+ *      '500':
+ *        description: Internal server error
+ *      '404':
+ *        description: User not found
+ *
+ */
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
@@ -33,6 +115,32 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /user/:
+ *  post:
+ *    summary: Adds user to DB
+ *    tags: [Users]
+ *    description: Create new user
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#components/schemas/User'
+ *    responses:
+ *      '201':
+ *        description: The User was succesfully created
+ *        contens:
+ *          application/json:
+ *           schema:
+ *                $ref: '#components/schemas/User'
+ *      '500':
+ *        description: Internal server error
+ *      '400':
+ *        description: User already exists
+ *
+ */
 router.post("/", checkEmail, async (req, res) => {
   const { name, surname, email } = req.body;
   const user = new User({
@@ -47,6 +155,33 @@ router.post("/", checkEmail, async (req, res) => {
     return res.status(500).send({ err: err.message });
   }
 });
+
+/**
+ * @swagger
+ * /user/:
+ *  patch:
+ *    summary: Edits user
+ *    tags: [Users]
+ *    description: Edit user in DB
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#components/schemas/User'
+ *    responses:
+ *      '200':
+ *        description: The User was succesfully updated
+ *        content:
+ *          application/json:
+ *           schema:
+ *                $ref: '#components/schemas/User'
+ *      '500':
+ *        description: Internal server error
+ *      '404':
+ *        description: User was not found
+ *
+ */
 router.patch("/", async (req, res) => {
   const { _id, name, surname, email } = req.body;
   try {
@@ -65,6 +200,34 @@ router.patch("/", async (req, res) => {
     return res.status(500).send({ err: err.message });
   }
 });
+
+/**
+ * @swagger
+ * /user/{id}:
+ *  delete:
+ *    summary: Removes user from DB
+ *    tags: [Users]
+ *    description: remove user from DB
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *    responses:
+ *      '200':
+ *        description: User Deleted
+ *        contens:
+ *          application/json:
+ *           schema:
+ *              items:
+ *                $ref: '#components/schemas/User'
+ *      '500':
+ *        description: Internal server error
+ *      '404':
+ *        description: User not found
+ *
+ */
 router.delete("/:id", async (req, res) => {
   const { id } = req.params;
   const user = await User.findByIdAndDelete(id);
